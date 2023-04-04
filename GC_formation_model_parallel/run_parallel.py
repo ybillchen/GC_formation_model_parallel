ecb6ac8fcc643a5ade539f6df0ec4c63fafef3d8
Bill Chen (Mac) <ybchen@umich.edu>
2023-04-04 15:21:15 -0400

    updated

--- GC_formation_model_parallel/run_parallel.py
+++ GC_formation_model_parallel/run_parallel.py
@@ -16,7 +16,7 @@ from .get_tid_parallel import get_tid_parallel
 
 __all__ = ['run_parallel']
 
-def run_serial(params, p):
+def run_serial(params, p, form=True, offset=True, assign=True):
 
     if params['verbose']:
         print('Runing model on %d halo(s) at process %d.'%(len(params['subs']),p))
@@ -30,15 +30,18 @@ def run_serial(params, p):
     run_params['cosmo'] = astro_utils.cosmo(h=run_params['h100'], 
         omega_baryon=run_params['Ob'], omega_matter=run_params['Om'])
 
-    form(run_params)
-    offset(run_params)
-    assign(run_params)
+    if form:
+        form(run_params)
+    if offset:
+        offset(run_params)
+    if assign:
+        assign(run_params)
 
     if params['verbose']:
         print('\nModel was run on %d halo(s) at process %d.\n'%(len(params['subs']),p))
 
-def run_parallel(params, Np=32, seed_based=False, stage1=True, stage2=True, skip=None):
-    if stage1:
+def run_parallel(params, Np=32, seed_based=False, form=True, offset=True, assign=True, tid=True, skip=None):
+    if form or offset or assign:
         run_params = copy(params)
 
         para_list = []
@@ -47,14 +50,14 @@ def run_parallel(params, Np=32, seed_based=False, stage1=True, stage2=True, skip
         if seed_based:
             for s in params['seed_list']:
                 run_params['seed'] = s
-                para_list.append((copy(run_params), p))
+                para_list.append((copy(run_params), p, form, offset, assign))
                 p += 1
         else:
             for p2 in params['p2_arr']:
                 for p3 in params['p3_arr']:
                     run_params['p2'] = p2
                     run_params['p3'] = p3
-                    para_list.append((copy(run_params), p))
+                    para_list.append((copy(run_params), p, form, offset, assign))
                     p += 1
 
         with Pool(Np) as p:
@@ -65,5 +68,5 @@ def run_parallel(params, Np=32, seed_based=False, stage1=True, stage2=True, skip
         # executor = MPIPoolExecutor(max_workers=Np)
         # executor.starmap(run_serial, para_list)
 
-    if stage2:
+    if tid:
         get_tid_parallel(params, Np, file_prefix = 'combine', seed_based=seed_based, skip=skip)
